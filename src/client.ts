4fc3b47f3bd54e035fa595a928d8c37de1ad894d
mostafa <14712022100624@stud.cu.edu.eg>
2025-06-26 15:47:16 +0300

    feat: encode command using RESP protocol

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,20 @@
 import net from "net"
+
+function encodeCommand(command: string): string {
+
+  let result = "";
+  const carridge = '\r\n'
+  const splitedCommand = command.split(/\s+/).filter(Boolean);
+  result = result.concat(`*${splitedCommand.length}`, carridge)
+  splitedCommand.forEach((word) => {
+    result = result.concat(`$${word.length}`, carridge, word, carridge)
+  })
+  return result;
+}
+
+
+
+
 const client = net.connect({ port: 8080 });
 client.on('data', (data) => {
   console.log(data.toString());
