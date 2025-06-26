4c80d5303d4e753c5dbdfa32614f04fada3c1b51
mostafa <14712022100624@stud.cu.edu.eg>
2025-06-26 15:49:27 +0300

    feat: parse command from client

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,24 @@
 
 import net from 'net'
+
+
+
+function formatCommand(command: string): string[] {
+
+  let formatedCommand = command.split('\r\n');
+  let num = Number(formatedCommand[0].slice(1))
+  let result = []
+  let index = 2;
+  for (let i = 0; i < num; i++) {
+    result.push(formatedCommand[index])
+    index += 2;
+  }
+  return result;
+
+}
+
+
+
 const server = net.createServer((socket) => { //'connection' listener
   console.log('client connected');
   socket.on('end', () => {
