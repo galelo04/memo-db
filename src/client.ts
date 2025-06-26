31546ccbbba5e7f115cc9a5a12fa8880f91dbec5
mostafa <14712022100624@stud.cu.edu.eg>
2025-06-26 15:44:37 +0300

    feat: add simple tcp socket client/server

---
+++ src/client.ts
@@ -0,0 +1,5 @@
+import net from "net"
+const client = net.connect({ port: 8080 });
+client.on('data', (data) => {
+  console.log(data.toString());
+});
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+
+import net from 'net'
+const server = net.createServer((socket) => { //'connection' listener
+  console.log('client connected');
+  socket.on('end', () => {
+    console.log('client disconnected');
+  });
+  socket.write('hello\r\n');
+});
+server.listen(8080, function() { //'listening' listener
+  console.log('server bound');
+});
