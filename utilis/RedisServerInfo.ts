98737d14664f4ab9115837cd34894d2e9b540dce
mostafa <14712022100624@stud.cu.edu.eg>
2025-07-04 11:54:15 +0300

    feat: add some other information about the server

--- utilis/RedisServerInfo.ts
+++ utilis/RedisServerInfo.ts
@@ -1,16 +1,29 @@
+import cuid from "cuid"
 type Role = "master" | "replica"
 export class RedisServerInfo {
   readonly role: Role
   readonly port: number
-  constructor(role: Role, port: number) {
+  readonly master_port: number
+  readonly master_host: string
+  readonly master_replid: string
+  readonly master_repl_offset: number
+  constructor(role: Role, port: number, master_port: number, master_host: string, master_replid: string, master_repl_offset: number) {
     this.role = role;
-    this.port = port
+    this.port = port;
+    this.master_port = master_port;
+    this.master_host = master_host;
+    this.master_replid = master_replid;
+    this.master_repl_offset = master_repl_offset;
   }
 }
 
 export class RedisServerInfoBuilder {
-  private _role: Role
-  private _port: number
+  private _role: Role;
+  private _port: number;
+  private _master_port: number;
+  private _master_host: string;
+  private _master_replid: string;
+  private _master_repl_offset: number;
   setRole(role: Role) {
     this._role = role
     return this
@@ -19,10 +32,27 @@ export class RedisServerInfoBuilder {
     this._port = port
     return this
   }
+  setMasterDetails(masterPort: number, masterHost: string) {
+    this._master_port = masterPort;
+    this._master_host = masterHost;
+    return this;
+  }
+  setMasterId() {
+    this._master_replid = cuid();
+    return this;
+  }
+  setMasterOffset(masterOffset: number) {
+    this._master_repl_offset = masterOffset;
+    return this;
+  }
   build() {
     return new RedisServerInfo(
       this._role,
-      this._port
+      this._port,
+      this._master_port,
+      this._master_host,
+      this._master_replid,
+      this._master_repl_offset
     )
   }
 }
