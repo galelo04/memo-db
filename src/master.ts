cd0c80efa56a87a350b242f7fcf6234fed35b779
mostafa <14712022100624@stud.cu.edu.eg>
2025-07-05 14:34:05 +0300

    fix: processBuffer back to the closure without passing args to mutatue buffer

--- src/master.ts
+++ src/master.ts
@@ -11,45 +11,6 @@ import { join } from 'path'
 import minimist from 'minimist'
 import cuid from 'cuid'
 
-async function processBuffer(buffer: Buffer, handleCommand: (command: string[]) => Promise<Response>, socket: Socket, redisServerInfo: RedisServerInfo, AOFdir?: string, AOFFileName?: string) {
-  while (true) {
-    let response: Response
-    if (buffer.length <= 0) {
-      break;
-    }
-    const result: tryParseResult = tryParse(buffer);
-    if (result.error) {
-      response = { type: ResponseType.error, data: [`${result.error}`] }
-      break;
-    }
-    if (!result.parsedCommand) {
-      break;
-    }
-    if (result.fullCommandText) {
-      if (AOFFileName && AOFdir) {
-        fsPromises.appendFile(join(AOFdir, AOFFileName), result.fullCommandText)
-        for (const replicaSocket of redisServerInfo.replicas) {
-          replicaSocket.write(result.fullCommandText)
-        }
-      }
-    }
-    buffer = result.remainingBuffer;
-    try {
-      if (result.parsedCommand[0] === "REPLCONF")
-        redisServerInfo.replicas.add(socket)
-      response = await handleCommand(result.parsedCommand);
-    } catch (err: any) {
-      response = { type: ResponseType.error, data: [`${err.message || err}`] }
-    }
-    const formated = formatResponse(response)
-    socket.write(formated)
-    if (formated.includes('FULLRESYNC') && AOFdir && AOFFileName) {
-      const content = await fsPromises.readFile(join(AOFdir, AOFFileName))
-      console.log('Sending AOF file content to replica')
-      socket.write(`$${content.length}\r\n${content}`)
-    }
-  }
-}
 
 
 async function main() {
@@ -66,11 +27,50 @@ async function main() {
     }
   }
   const server = net.createServer((socket) => {
+    async function processBuffer() {
+      while (true) {
+        let response: Response
+        if (buffer.length <= 0) {
+          break;
+        }
+        const result: tryParseResult = tryParse(buffer);
+        if (result.error) {
+          response = { type: ResponseType.error, data: [`${result.error}`] }
+          break;
+        }
+        if (!result.parsedCommand) {
+          break;
+        }
+        if (result.fullCommandText) {
+          if (AOFFileName && AOFdir) {
+            fsPromises.appendFile(join(AOFdir, AOFFileName), result.fullCommandText)
+            for (const replicaSocket of redisServerInfo.replicas) {
+              replicaSocket.write(result.fullCommandText)
+            }
+          }
+        }
+        buffer = result.remainingBuffer;
+        try {
+          if (result.parsedCommand[0] === "REPLCONF")
+            redisServerInfo.replicas.add(socket)
+          response = await handleCommand(result.parsedCommand);
+        } catch (err: any) {
+          response = { type: ResponseType.error, data: [`${err.message || err}`] }
+        }
+        const formated = formatResponse(response)
+        socket.write(formated)
+        if (formated.includes('FULLRESYNC') && AOFdir && AOFFileName) {
+          const content = await fsPromises.readFile(join(AOFdir, AOFFileName))
+          console.log('Sending AOF file content to replica')
+          socket.write(`$${content.length}\r\n${content}`)
+        }
+      }
+    }
     let buffer = Buffer.alloc(0)
     console.log('client connected');
     socket.on('data', (data) => {
       buffer = Buffer.concat([buffer, data])
-      processBuffer(buffer, handleCommand, socket, redisServerInfo, AOFdir, AOFFileName)
+      processBuffer()
     })
   })
 
